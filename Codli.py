c27c2412772e65af86281bf45bd7e6753bacb7e8
Shivansh Goel <44195921+blackphin@users.noreply.github.com>
2020-07-23 00:24:45 +0530

    Add files via upload

---
+++ Codli.py
@@ -0,0 +1,287 @@
+print("Welcome to Codly 2.2.0".center(110))
+for z in range (1,5,1):
+    a=input("Enter ID: ").lower()
+    b=input("Enter Password: ").lower()
+    if (a=="shivanshgoel534" or a=="shivanshgoel009" or a=="blackphin") and b=="tbmlsktt":
+        for y in range (1,50,1):
+            print()
+            print()
+            print("What do you want to do:")
+            print("1. Encrypt")
+            print("2. Decrypt")
+            c=input("Enter Option Number: ")
+            print()
+            if (c=="1" or c=="1." or c!="ENCRYPTER"):
+                str=input("Enter the text to be Encrypted: ").lower()
+                print()
+                for i in str:
+                    if i=="a":
+                        print("^",end="")
+                    elif i=="b":
+                        print(")",end="")
+                    elif i=="c":
+                        print("(",end="")
+                    elif i=="d":
+                        print("#",end="")
+                    elif i=="e":
+                        print("!",end="")
+                    elif i=="f":
+                        print("@",end="")
+                    elif i=="g":
+                        print("%",end="")
+                    elif i=="h":
+                        print("$",end="")
+                    elif i=="i":
+                        print("&",end="")
+                    elif i=="j":
+                        print("*",end="")
+                    elif i=="k":
+                        print("-",end="")
+                    elif i=="l":
+                        print("=",end="")
+                    elif i=="m":
+                        print("_",end="")
+                    elif i=="n":
+                        print("+",end="")
+                    elif i=="o":
+                        print("/",end="")
+                    elif i=="p":
+                        print("?",end="")
+                    elif i=="q":
+                        print("'",end="")
+                    elif i=="r":
+                        print("]",end="")
+                    elif i=="s":
+                        print("}",end="")
+                    elif i=="t":
+                        print("[",end="")
+                    elif i=="u":
+                        print("{",end="")
+                    elif i=="v":
+                        print(";",end="")
+                    elif i=="w":
+                        print(":",end="")
+                    elif i=="x":
+                        print(".",end="")
+                    elif i=="y":
+                        print(">",end="")
+                    elif i=="z":
+                        print(",",end="")
+                    elif i==" ":
+                        print("<",end="")
+                    elif i=="1":
+                        print("0",end="")
+                    elif i=="2":
+                        print("9",end="")
+                    elif i=="3":
+                        print("8",end="")
+                    elif i=="4":
+                        print("7",end="")
+                    elif i=="5":
+                        print("6",end="")
+                    elif i=="6":
+                        print("5",end="")
+                    elif i=="7":
+                        print("4",end="")
+                    elif i=="8":
+                        print("3",end="")
+                    elif i=="9":
+                        print("2",end="")
+                    elif i=="0":
+                        print("1",end="")
+                    elif i=="!":
+                        print("q",end="")
+                    elif i=="@":
+                        print("w",end="")
+                    elif i=="#":
+                        print("e",end="")
+                    elif i=="$":
+                        print("r",end="")
+                    elif i=="%":
+                        print("t",end="")
+                    elif i=="^":
+                        print("y",end="")
+                    elif i=="&":
+                        print("u",end="")
+                    elif i=="*":
+                        print("i",end="")
+                    elif i=="(":
+                        print("o",end="")
+                    elif i==")":
+                        print("p",end="")
+                    elif i=="-":
+                        print("a",end="")
+                    elif i=="_":
+                        print("s",end="")
+                    elif i=="=":
+                        print("d",end="")
+                    elif i=="+":
+                        print("f",end="")
+                    elif i=="/":
+                        print("g",end="")
+                    elif i=="'":
+                        print("h",end="")
+                    elif i=="]":
+                        print("j",end="")
+                    elif i=="[":
+                        print("k",end="")
+                    elif i==";":
+                        print("l",end="")
+                    elif i==".":
+                        print("z",end="")
+                    elif i==",":
+                        print("x",end="")
+                    elif i=="<":
+                        print("c",end="")
+                    elif i==">":
+                        print("v",end="")
+                    elif i==":":
+                        print("b",end="")
+                    elif i=="{":
+                        print("n",end="")
+                    elif i=="}":
+                        print("m",end="")
+                    elif i=="?":
+                        print("`",end="")
+                    else:
+                        print("Invalid Character")
+            elif (c=="2" or c=="2." or c!="DECRYPTER"):
+                str=input("Enter the text to be Decrypted: ")
+                print()
+                for g in str:
+                    if g=="^":
+                        print("a",end="")
+                    elif g==")":
+                        print("b",end="")
+                    elif g=="(":
+                        print("c",end="")
+                    elif g=="#":
+                        print("d",end="")
+                    elif g=="!":
+                        print("e",end="")
+                    elif g=="@":
+                        print("f",end="")
+                    elif g=="%":
+                        print("g",end="")
+                    elif g=="$":
+                        print("h",end="")
+                    elif g=="&":
+                        print("i",end="")
+                    elif g=="*":
+                        print("j",end="")
+                    elif g=="-":
+                        print("k",end="")
+                    elif g=="=":
+                        print("l",end="")
+                    elif g=="_":
+                        print("m",end="")
+                    elif g=="+":
+                        print("n",end="")
+                    elif g=="/":
+                        print("o",end="")
+                    elif g=="?":
+                        print("p",end="")
+                    elif g=="'":
+                        print("q",end="")
+                    elif g=="]":
+                        print("r",end="")
+                    elif g=="}":
+                        print("s",end="")
+                    elif g=="[":
+                        print("t",end="")
+                    elif g=="{":
+                        print("u",end="")
+                    elif g==";":
+                        print("v",end="")
+                    elif g==":":
+                        print("w",end="")
+                    elif g==".":
+                        print("x",end="")
+                    elif g==">":
+                        print("y",end="")
+                    elif g==",":
+                        print("z",end="")
+                    elif g=="<":
+                        print(" ",end="")
+                    elif g=="0":
+                        print("1",end="")
+                    elif g=="9":
+                        print("2",end="")
+                    elif g=="8":
+                        print("3",end="")
+                    elif g=="7":
+                        print("4",end="")
+                    elif g=="6":
+                        print("5",end="")
+                    elif g=="5":
+                        print("6",end="")
+                    elif g=="4":
+                        print("7",end="")
+                    elif g=="3":
+                        print("8",end="")
+                    elif g=="2":
+                        print("9",end="")
+                    elif g=="1":
+                        print("0",end="")
+                    elif g=="q":
+                        print("!",end="")
+                    elif g=="w":
+                        print("@",end="")
+                    elif g=="e":
+                        print("#",end="")
+                    elif g=="r":
+                        print("$",end="")
+                    elif g=="t":
+                        print("%",end="")
+                    elif g=="y":
+                        print("^",end="")
+                    elif g=="u":
+                        print("&",end="")
+                    elif g=="i":
+                        print("*",end="")
+                    elif g=="o":
+                        print("(",end="")
+                    elif g=="p":
+                        print(")",end="")
+                    elif g=="a":
+                        print("-",end="")
+                    elif g=="s":
+                        print("_",end="")
+                    elif g=="d":
+                        print("=",end="")
+                    elif g=="f":
+                        print("+",end="")
+                    elif g=="g":
+                        print("/",end="")
+                    elif g=="h":
+                        print("'",end="")
+                    elif g=="j":
+                        print("]",end="")
+                    elif g=="k":
+                        print("[",end="")
+                    elif g=="l":
+                        print(";",end="")
+                    elif g=="z":
+                        print(".",end="")
+                    elif g=="x":
+                        print(",",end="")
+                    elif g=="c":
+                        print("<",end="")
+                    elif g=="v":
+                        print(">",end="")
+                    elif g=="b":
+                        print(":",end="")
+                    elif g=="n":
+                        print("{",end="")
+                    elif g=="m":
+                        print("}",end="")
+                    elif g=="`":
+                        print("?",end="")
+                    else:
+                        print("Invalid Character")
+            else:
+                print("Invalid Choice")
+    elif a=="shivanshgoel534" or a=="shivanshgoel009" or a=="blackphin":
+        print("Wrong Password, Try Again".center(110))
+    else:
+        print("Wrong ID/Password, Try Again".center(110))
